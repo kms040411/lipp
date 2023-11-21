f13d107dc1fc65d6a654bdfe9381b2625597af98
mskim <mskim@casys.kaist.ac.kr>
2023-11-21 16:39:04 +0900

    .

--- src/core/lipp.h
+++ src/core/lipp.h
@@ -1,7 +1,7 @@
 #ifndef __LIPP_H__
 #define __LIPP_H__
 
-#include "lipp_base.h"
+#include "lipp_base_string.h"
 #include <stdint.h>
 #include <math.h>
 #include <limits>
@@ -38,11 +38,9 @@ typedef uint16_t bitmap_t;
 #include <chrono>
 #endif
 
-template<class T, class P, bool USE_FMCD = true>
+template<class T, class P, int LEN>
 class LIPP
 {
-    static_assert(std::is_arithmetic<T>::value, "LIPP key type must be numeric.");
-
     inline int compute_gap_count(int size) {
         return 5;
     }
@@ -58,13 +56,9 @@ class LIPP
         }
         return std::min(node->num_items - 1, static_cast<int>(v));
     }
-
-    const double BUILD_LR_REMAIN;
     const bool QUIET;
 
     struct {
-        long long fmcd_success_times = 0;
-        long long fmcd_broken_times = 0;
         #if COLLECT_TIME
         double time_scan_and_destory_tree = 0;
         double time_build_tree_bulk = 0;
@@ -74,8 +68,8 @@ class LIPP
 public:
     typedef std::pair<T, P> V;
 
-    LIPP(double BUILD_LR_REMAIN = 0, bool QUIET = true)
-        : BUILD_LR_REMAIN(BUILD_LR_REMAIN), QUIET(QUIET) {
+    LIPP(bool QUIET = true)
+        : QUIET(QUIET) {
         {
             std::vector<Node*> nodes;
             for (int _ = 0; _ < 1e7; _ ++) {
@@ -88,9 +82,6 @@ public:
             if (!QUIET) {
                 printf("initial memory pool size = %lu\n", pending_two.size());
             }
-        }
-        if (USE_FMCD && !QUIET) {
-            printf("enable FMCD\n");
         }
 
         root = build_tree_none();
@@ -184,61 +175,6 @@ public:
         return range_core_len<false>(results, 0, root, lower, len);
     }
 
-    void show() const {
-        printf("============= SHOW LIPP ================\n");
-
-        std::stack<Node*> s;
-        s.push(root);
-        while (!s.empty()) {
-            Node* node = s.top(); s.pop();
-
-            printf("Node(%p, a = %lf, b = %lf, num_items = %d)", node, node->model.a, node->model.b, node->num_items);
-            printf("[");
-            int first = 1;
-            for (int i = 0; i < node->num_items; i ++) {
-                if (!first) {
-                    printf(", ");
-                }
-                first = 0;
-                if (BITMAP_GET(node->none_bitmap, i) == 1) {
-                    printf("None");
-                } else if (BITMAP_GET(node->child_bitmap, i) == 0) {
-                    std::stringstream s;
-                    s << node->items[i].comp.data.key;
-                    printf("Key(%s)", s.str().c_str());
-                } else {
-                    printf("Child(%p)", node->items[i].comp.child);
-                    s.push(node->items[i].comp.child);
-                }
-            }
-            printf("]\n");
-        }
-    }
-    void print_depth() const {
-        std::stack<Node*> s;
-        std::stack<int> d;
-        s.push(root);
-        d.push(1);
-
-        int max_depth = 1;
-        int sum_depth = 0, sum_nodes = 0;
-        while (!s.empty()) {
-            Node* node = s.top(); s.pop();
-            int depth = d.top(); d.pop();
-            for (int i = 0; i < node->num_items; i ++) {
-                if (BITMAP_GET(node->child_bitmap, i) == 1) {
-                    s.push(node->items[i].comp.child);
-                    d.push(depth + 1);
-                } else if (BITMAP_GET(node->none_bitmap, i) != 1) {
-                    max_depth = std::max(max_depth, depth);
-                    sum_depth += depth;
-                    sum_nodes ++;
-                }
-            }
-        }
-
-        printf("max_depth = %d, avg_depth = %.2lf\n", max_depth, double(sum_depth) / double(sum_nodes));
-    }
     void verify() const {
         std::stack<Node*> s;
         s.push(root);
@@ -257,17 +193,7 @@ public:
             RT_ASSERT(sum_size == node->size);
         }
     }
-    void print_stats() const {
-        printf("======== Stats ===========\n");
-        if (USE_FMCD) {
-            printf("\t fmcd_success_times = %lld\n", stats.fmcd_success_times);
-            printf("\t fmcd_broken_times = %lld\n", stats.fmcd_broken_times);
-        }
-        #if COLLECT_TIME
-        printf("\t time_scan_and_destory_tree = %lf\n", stats.time_scan_and_destory_tree);
-        printf("\t time_build_tree_bulk = %lf\n", stats.time_build_tree_bulk);
-        #endif
-    }
+
     size_t index_size(bool total=false, bool ignore_child=true) const {
         std::stack<Node*> s;
         s.push(root);
@@ -318,7 +244,7 @@ private:
         int fixed; // fixed node will not trigger rebuild
         int num_inserts, num_insert_to_data;
         int num_items; // size of items
-        LinearModel<T> model;
+        LinearModel<T, LEN> model;
         Item* items;
         bitmap_t* none_bitmap; // 1 means None, 0 means Data or Child
         bitmap_t* child_bitmap; // 1 means Child. will always be 0 when none_bitmap is 1
@@ -373,7 +299,6 @@ private:
         node->fixed = 0;
         node->num_inserts = node->num_insert_to_data = 0;
         node->num_items = 1;
-        node->model.a = node->model.b = 0;
         node->items = new_items(1);
         node->none_bitmap = new_bitmap(1);
         node->none_bitmap[0] = BITMAP_ONE;
@@ -411,16 +336,14 @@ private:
             node = pending_two.top(); pending_two.pop();
         }
 
-        const long double mid1_key = key1;
-        const long double mid2_key = key2;
-
         const double mid1_target = node->num_items / 3;
         const double mid2_target = node->num_items * 2 / 3;
 
-        node->model.a = (mid2_target - mid1_target) / (mid2_key - mid1_key);
-        node->model.b = mid1_target - node->model.a * mid1_key;
-        RT_ASSERT(isfinite(node->model.a));
-        RT_ASSERT(isfinite(node->model.b));
+        // TODO:
+        std::vector<std::pair<T, size_t>> train_data;
+        train_data.push_back(std::make_pair(key1, mid1_target));
+        train_data.push_back(std::make_pair(key2, mid2_target));
+        node->model.train(train_data);
 
         { // insert key1&value1
             int pos = PREDICT_POS(node, key1);
@@ -442,16 +365,6 @@ private:
     /// bulk build, _keys must be sorted in asc order.
     Node* build_tree_bulk(T* _keys, P* _values, int _size)
     {
-        if (USE_FMCD) {
-            return build_tree_bulk_fmcd(_keys, _values, _size);
-        } else {
-            return build_tree_bulk_fast(_keys, _values, _size);
-        }
-    }
-    /// bulk build, _keys must be sorted in asc order.
-    /// split keys into three parts at each node.
-    Node* build_tree_bulk_fast(T* _keys, P* _values, int _size)
-    {
         RT_ASSERT(_size > 1);
 
         typedef struct {
@@ -489,180 +402,36 @@ private:
                 node->fixed = 0;
                 node->num_inserts = node->num_insert_to_data = 0;
 
-                int mid1_pos = (size - 1) / 3;
-                int mid2_pos = (size - 1) * 2 / 3;
-
-                RT_ASSERT(0 <= mid1_pos);
-                RT_ASSERT(mid1_pos < mid2_pos);
-                RT_ASSERT(mid2_pos < size - 1);
-
-                const long double mid1_key =
-                        (static_cast<long double>(keys[mid1_pos]) + static_cast<long double>(keys[mid1_pos + 1])) / 2;
-                const long double mid2_key =
-                        (static_cast<long double>(keys[mid2_pos]) + static_cast<long double>(keys[mid2_pos + 1])) / 2;
-
-                node->num_items = size * static_cast<int>(BUILD_GAP_CNT + 1);
-                const double mid1_target = mid1_pos * static_cast<int>(BUILD_GAP_CNT + 1) + static_cast<int>(BUILD_GAP_CNT + 1) / 2;
-                const double mid2_target = mid2_pos * static_cast<int>(BUILD_GAP_CNT + 1) + static_cast<int>(BUILD_GAP_CNT + 1) / 2;
-
-                node->model.a = (mid2_target - mid1_target) / (mid2_key - mid1_key);
-                node->model.b = mid1_target - node->model.a * mid1_key;
-                RT_ASSERT(isfinite(node->model.a));
-                RT_ASSERT(isfinite(node->model.b));
-
-                const int lr_remains = static_cast<int>(size * BUILD_LR_REMAIN);
-                node->model.b += lr_remains;
-                node->num_items += lr_remains * 2;
-
-                if (size > 1e6) {
-                    node->fixed = 1;
+                // TODO:
+                std::vector<std::pair<T, size_t>> train_data;
+                for (size_t idx=0; idx<size; idx++) {
+                    train_data.push_back(std::make_pair(keys[idx], idx * static_cast<int>(BUILD_GAP_CNT + 1) + static_cast<int>(BUILD_GAP_CNT + 1) / 2));
                 }
+                node->model.train(train_data);
+                // int mid1_pos = (size - 1) / 3;
+                // int mid2_pos = (size - 1) * 2 / 3;
 
-                node->items = new_items(node->num_items);
-                const int bitmap_size = BITMAP_SIZE(node->num_items);
-                node->none_bitmap = new_bitmap(bitmap_size);
-                node->child_bitmap = new_bitmap(bitmap_size);
-                memset(node->none_bitmap, 0xff, sizeof(bitmap_t) * bitmap_size);
-                memset(node->child_bitmap, 0, sizeof(bitmap_t) * bitmap_size);
+                // RT_ASSERT(0 <= mid1_pos);
+                // RT_ASSERT(mid1_pos < mid2_pos);
+                // RT_ASSERT(mid2_pos < size - 1);
 
-                for (int item_i = PREDICT_POS(node, keys[0]), offset = 0; offset < size; ) {
-                    int next = offset + 1, next_i = -1;
-                    while (next < size) {
-                        next_i = PREDICT_POS(node, keys[next]);
-                        if (next_i == item_i) {
-                            next ++;
-                        } else {
-                            break;
-                        }
-                    }
-                    if (next == offset + 1) {
-                        BITMAP_CLEAR(node->none_bitmap, item_i);
-                        node->items[item_i].comp.data.key = keys[offset];
-                        node->items[item_i].comp.data.value = values[offset];
-                    } else {
-                        // ASSERT(next - offset <= (size+2) / 3);
-                        BITMAP_CLEAR(node->none_bitmap, item_i);
-                        BITMAP_SET(node->child_bitmap, item_i);
-                        node->items[item_i].comp.child = new_nodes(1);
-                        s.push((Segment){begin + offset, begin + next, level + 1, node->items[item_i].comp.child});
-                    }
-                    if (next >= size) {
-                        break;
-                    } else {
-                        item_i = next_i;
-                        offset = next;
-                    }
-                }
-            }
-        }
+                // const long double mid1_key =
+                //         (static_cast<long double>(keys[mid1_pos]) + static_cast<long double>(keys[mid1_pos + 1])) / 2;
+                // const long double mid2_key =
+                //         (static_cast<long double>(keys[mid2_pos]) + static_cast<long double>(keys[mid2_pos + 1])) / 2;
 
-        return ret;
-    }
-    /// bulk build, _keys must be sorted in asc order.
-    /// FMCD method.
-    Node* build_tree_bulk_fmcd(T* _keys, P* _values, int _size)
-    {
-        RT_ASSERT(_size > 1);
+                // node->num_items = size * static_cast<int>(BUILD_GAP_CNT + 1);
+                // const double mid1_target = mid1_pos * static_cast<int>(BUILD_GAP_CNT + 1) + static_cast<int>(BUILD_GAP_CNT + 1) / 2;
+                // const double mid2_target = mid2_pos * static_cast<int>(BUILD_GAP_CNT + 1) + static_cast<int>(BUILD_GAP_CNT + 1) / 2;
 
-        typedef struct {
-            int begin;
-            int end;
-            int level; // top level = 1
-            Node* node;
-        } Segment;
-        std::stack<Segment> s;
+                // node->model.a = (mid2_target - mid1_target) / (mid2_key - mid1_key);
+                // node->model.b = mid1_target - node->model.a * mid1_key;
+                // RT_ASSERT(isfinite(node->model.a));
+                // RT_ASSERT(isfinite(node->model.b));
 
-        Node* ret = new_nodes(1);
-        s.push((Segment){0, _size, 1, ret});
-
-        while (!s.empty()) {
-            const int begin = s.top().begin;
-            const int end = s.top().end;
-            const int level = s.top().level;
-            Node* node = s.top().node;
-            s.pop();
-
-            RT_ASSERT(end - begin >= 2);
-            if (end - begin == 2) {
-                Node* _ = build_tree_two(_keys[begin], _values[begin], _keys[begin+1], _values[begin+1]);
-                memcpy(node, _, sizeof(Node));
-                delete_nodes(_, 1);
-            } else {
-                T* keys = _keys + begin;
-                P* values = _values + begin;
-                const int size = end - begin;
-                const int BUILD_GAP_CNT = compute_gap_count(size);
-
-                node->is_two = 0;
-                node->build_size = size;
-                node->size = size;
-                node->fixed = 0;
-                node->num_inserts = node->num_insert_to_data = 0;
-
-                // FMCD method
-                // Here the implementation is a little different with Algorithm 1 in our paper.
-                // In Algorithm 1, U_T should be (keys[size-1-D] - keys[D]) / (L - 2).
-                // But according to the derivation described in our paper, M.A should be less than 1 / U_T.
-                // So we added a small number (1e-6) to U_T.
-                // In fact, it has only a negligible impact of the performance.
-                {
-                    const int L = size * static_cast<int>(BUILD_GAP_CNT + 1);
-                    int i = 0;
-                    int D = 1;
-                    RT_ASSERT(D <= size-1-D);
-                    double Ut = (static_cast<long double>(keys[size - 1 - D]) - static_cast<long double>(keys[D])) /
-                                (static_cast<double>(L - 2)) + 1e-6;
-                    while (i < size - 1 - D) {
-                        while (i + D < size && keys[i + D] - keys[i] >= Ut) {
-                            i ++;
-                        }
-                        if (i + D >= size) {
-                            break;
-                        }
-                        D = D + 1;
-                        if (D * 3 > size) break;
-                        RT_ASSERT(D <= size-1-D);
-                        Ut = (static_cast<long double>(keys[size - 1 - D]) - static_cast<long double>(keys[D])) /
-                             (static_cast<double>(L - 2)) + 1e-6;
-                    }
-                    if (D * 3 <= size) {
-                        stats.fmcd_success_times ++;
-
-                        node->model.a = 1.0 / Ut;
-                        node->model.b = (L - node->model.a * (static_cast<long double>(keys[size - 1 - D]) +
-                                                              static_cast<long double>(keys[D]))) / 2;
-                        RT_ASSERT(isfinite(node->model.a));
-                        RT_ASSERT(isfinite(node->model.b));
-                        node->num_items = L;
-                    } else {
-                        stats.fmcd_broken_times ++;
-
-                        int mid1_pos = (size - 1) / 3;
-                        int mid2_pos = (size - 1) * 2 / 3;
-
-                        RT_ASSERT(0 <= mid1_pos);
-                        RT_ASSERT(mid1_pos < mid2_pos);
-                        RT_ASSERT(mid2_pos < size - 1);
-
-                        const long double mid1_key = (static_cast<long double>(keys[mid1_pos]) +
-                                                      static_cast<long double>(keys[mid1_pos + 1])) / 2;
-                        const long double mid2_key = (static_cast<long double>(keys[mid2_pos]) +
-                                                      static_cast<long double>(keys[mid2_pos + 1])) / 2;
-
-                        node->num_items = size * static_cast<int>(BUILD_GAP_CNT + 1);
-                        const double mid1_target = mid1_pos * static_cast<int>(BUILD_GAP_CNT + 1) + static_cast<int>(BUILD_GAP_CNT + 1) / 2;
-                        const double mid2_target = mid2_pos * static_cast<int>(BUILD_GAP_CNT + 1) + static_cast<int>(BUILD_GAP_CNT + 1) / 2;
-
-                        node->model.a = (mid2_target - mid1_target) / (mid2_key - mid1_key);
-                        node->model.b = mid1_target - node->model.a * mid1_key;
-                        RT_ASSERT(isfinite(node->model.a));
-                        RT_ASSERT(isfinite(node->model.b));
-                    }
-                }
-                RT_ASSERT(node->model.a >= 0);
-                const int lr_remains = static_cast<int>(size * BUILD_LR_REMAIN);
-                node->model.b += lr_remains;
-                node->num_items += lr_remains * 2;
+                // const int lr_remains = static_cast<int>(size * BUILD_LR_REMAIN);
+                // node->model.b += lr_remains;
+                // node->num_items += lr_remains * 2;
 
                 if (size > 1e6) {
                     node->fixed = 1;
---
+++ src/core/lipp_base_string.h
@@ -0,0 +1,78 @@
+#ifndef __LIPP_BASE_H__
+#define __LIPP_BASE_H__
+
+#include <limits>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+
+#include "mkl.h"
+#include "mkl_lapacke.h"
+
+#define USE_BIAS 1
+
+// Linear regression model
+template <class T, int LEN>
+class LinearModel
+{
+public:
+    double model[LEN + USE_BIAS] = {0.0}; // Model
+
+    LinearModel() = default;
+    LinearModel(double *other_model): {
+        for(size_t i=0; i<LEN + USE_BIAS; i++) {
+            model[i] = other_model[i];
+        }
+    }
+    explicit LinearModel(const LinearModel &other) {
+        for(size_t i=0; i<LEN + 1; i++) {
+            model[i] = other.model[i];
+        }
+    }
+
+    inline double predict_double(T key) const
+    {
+        double res = 0;
+        for (size_t i=0; i<LEN; i++) {
+            res += model[i] * (double)(key[i]);
+        }
+        if (USE_BIAS) {
+            res += model[LEN + USE_BIAS - 1];
+        }
+        return res;
+    }
+
+    inline int predict(T key) const
+    {
+        return std::floor(predict_double(key));
+    }
+
+    inline void train(std::vector<std::pair<T, size_t>> key_idx&) {
+        int m = key_idx.size();
+        int n = USE_BIAS ? LEN + 1 : LEN;
+        double *a = (double *) malloc(sizeof(double) * m * n);
+        double *b = (double *) malloc(sizeof(double) * m);
+        for (size_t i=0; i<m; i++) {
+            for (size_t j=0; j<LEN; j++) {
+                a[i * n + j] = (double)(key_idx[i].first[j]);
+            }
+            if (USE_BIAS) a[i * n + LEN] = 1.0;
+            b[i] = (double)(key_idx[i].second);
+        }
+
+        LAPACKE_dgels(LAPACK_ROW_MAJOR, 'N', m, n, 1, a, n, b, 1);
+        
+        for (size_t j=0; j<LEN; j++) {
+            model[j] = b[j];
+        }
+        if (USE_BIAS) model[LEN] = b[LEN];
+
+        free(a);
+        free(b);
+
+        return;
+    };
+
+};
+
+#endif // __LIPP_BASE_H__
